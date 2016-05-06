5988871b5d121c12c8256cb66c54772ad9aa69a4
dollop <dollop@gmail.com>
2016-05-06 13:24:35 -0700

    @1:25pm from Tower

--- HackingWithSwift_Project02/ViewController.swift
+++ HackingWithSwift_Project02/ViewController.swift
@@ -13,17 +13,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+ 
+    var countries = [String]()
+    var score = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        countries.append("estonia")
+        
+        countries.append("france")
+        
+        countries.append("germany")
+        
+        countries.append("ireland")
+        
+        countries.append("italy")
+        
+        countries.append("monocao")
+        
+        countries.append("nigeria")
+        
+        countries.append("poland")
+        
+        countries.append("russia")
+        
+        countries.append("spain")
+        
+        countries.append("uk")
+        
+        countries.append("us")
+        
+        askQuestion ()
+        
     }
+    
+        func askQuestion () {
+            
+            button1.setImage (UIImage (named: countries[0]), forState: .Normal)
+            
+            button2.setImage (UIImage (named: countries[1]), forState: .Normal)
+            
+            button3.setImage (UIImage (named: countries[2]), forState: .Normal)
+        }
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+ 
 }
 
